878108a3f445c9b813e8f2b94d4d7f4f4f1694e4
starwalker8p <starwalker8p@gmail.com>
2023-10-16 14:44:12 -0300

    fiz a atividade

---
+++ maior-menor.py
@@ -0,0 +1,7 @@
+numero1,numero2=float(input("numero 1: ")), float(input("numero 2: "))
+if numero1>numero2:
+    print("numero 1 maior que numero 2")
+elif numero2>numero1:
+    print("numero 2 é maior que o numero 1")
+else:
+    print("os numeros são iguais")
